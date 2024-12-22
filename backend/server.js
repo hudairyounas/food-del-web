f5adb2889092521afc2ca9339d993b767e289401
Hudair Younis <hudairshorts@gmail.com>
2024-12-22 15:42:36 +0500

    cors change in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,41 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config.js'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
+const corsOptions = {
+  origin: [
+    "https://food-del-web.vercel.app",
+    "https://food-del-web-admin.vercel.app",
+  ],
+};
 
 //? app config
 const app = express();
 const port = process.env.PORT || 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors(corsOptions));
 
 // db connection
 connectDB();
 
 //? api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static("uploads"))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
-
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Working")
-})
+  res.send("API Working");
+});
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
-})
+  console.log(`Server Started on http://localhost:${port}`);
+});
